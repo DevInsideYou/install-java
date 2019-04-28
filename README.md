c1db475a94f8f4720c6a353297fbbb51133a4108
Vladyslav Pekker <agilesteel@gmail.com>
2019-04-28 11:45:02 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installs [JRE & JDK 8](https://openjdk.java.net/) on Ubuntu
+
+```bash
+wget -q https://raw.githubusercontent.com/DevInsideYou/install-java/master/install-java.sh
+sudo chmod +x install-java.sh && ./install-java.sh
+```
---
+++ install-java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# install jre
+wget -q https://raw.githubusercontent.com/DevInsideYou/install-jre/master/install-jre.sh
+sudo chmod +x install-jre.sh && ./install-jre.sh
+
+# install jdk
+wget -q https://raw.githubusercontent.com/DevInsideYou/install-jdk/master/install-jdk.sh
+sudo chmod +x install-jdk.sh && ./install-jdk.sh
+
+# remove yourself
+# rm $0
